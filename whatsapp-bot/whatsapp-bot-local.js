09bd4a314768311faccb2e02916098661232b53f
ghiffaribraviah <ghiffari.bravia.hisham@gmail.com>
2025-08-03 17:34:23 +0700

    feat: local auth

---
+++ whatsapp-bot/whatsapp-bot-local.js
@@ -0,0 +1,69 @@
+import { wa_text, wa_text_update } from "../spreadsheet/text-local.js";
+import 'dotenv/config';
+import wapkg from 'whatsapp-web.js';
+import qrpkg from 'qrcode-terminal';
+import cron from 'node-cron';
+const { Client, LocalAuth } = wapkg;
+const qrcode = qrpkg;
+
+async function dailyReminder(){
+    cron.schedule('0 */2 * * *', async() => {
+        const number = process.env.TARGET_NUMBER_ID;
+        const chatId = number.includes('@g.us') ? number : number.replace('+', '') + '@c.us';
+        client.sendMessage(chatId, wa_text);
+     });
+
+}
+
+const puppeteerOptions = {
+     headless: true,
+     args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+     ],
+     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+ };
+
+let isReady = false;
+
+const client = new Client({
+        puppeteer: puppeteerOptions,
+        authStrategy: new LocalAuth({
+            dataPath: "wa_data"
+        })
+    });
+
+client.on("qr", (qr) => {
+    console.log("⚡ Scan this QR code:");
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', (session) => {
+    console.log('✅ AUTHENTICATED');
+});
+
+client.on('auth_failure', (msg) => {
+    console.error('❌ AUTHENTICATION FAILURE', msg);
+});
+
+client.on('disconnected', (reason) => {
+    console.log('❌ Client was disconnected', reason);
+});
+
+client.once("ready", () => {
+    isReady = true;
+    console.log("✅ Client is ready!");
+    dailyReminder();
+});
+
+client.on('message_create', message => {
+    if(message.body === "!RekapSekre"){
+        console.log("Requested Rekap from: " + message.from)
+        client.sendMessage(message.from, wa_text);
+    }
+});
+
+wa_text_update();
+
+
+client.initialize();
