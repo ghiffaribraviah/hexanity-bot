869090dc8ffc6b9d1fc4c6d9f37c31e910ef08b9
Raihan Putra Kirana <me@raihanpk.com>
2025-09-15 11:13:10 +0700

    Setup pm2 untuk background process dan autorestart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,20 @@ module.exports = {
       name: "hexanity-bot",
       script: "index.js",
       watch: false,
-      env: { NODE_ENV: "production" }
+      autorestart: true,
+      instances: 1,
+      exec_mode: "fork",
+      kill_timeout: 3000,
+      time: true,
+      merge_logs: true,
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
   ]
 };
